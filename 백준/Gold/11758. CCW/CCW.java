9d9c2180f62acd33606fcf34d53e1170ae603249
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2023-08-19 12:20:34 +0900

    [Gold V] Title: CCW, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Gold/11758. CCW/CCW.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x1= sc.nextInt();
+		int y1= sc.nextInt();
+		int x2= sc.nextInt();
+		int y2= sc.nextInt();
+		int x3= sc.nextInt();
+		int y3= sc.nextInt();
+		
+		int result = x1*y2+x2*y3+x3*y1-x2*y1-x3*y2-x1*y3;
+		if(result>0) {
+			System.out.print(1);
+		}else if(result==0) {
+			System.out.print(0);
+		}else {
+			System.out.print(-1);
+		}
+
+	}
+
+}
