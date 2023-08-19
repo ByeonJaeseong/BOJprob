8821e08a82bc25a009ea28a0e43805048febebbd
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2023-08-19 12:12:54 +0900

    [Bronze V] Title: 시험 성적, Time: 232 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=90&&100>=a){
+            System.out.println("A");}
+        else if (a>=80&&89>=a){
+            System.out.println("B");}
+        else if (a>=70&&79>=a){
+            System.out.println("C");}
+        else if (a>=60&&69>=a){
+            System.out.println("D");}
+        else{
+            System.out.println("F");}
+        }
+}
+            
+           
