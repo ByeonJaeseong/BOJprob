fa0e9a6a5b84d6f005cbd0c7084dab9c6a546515
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2023-08-19 12:18:20 +0900

    [Silver IV] Title: ATM, Time: 284 ms, Memory: 20780 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N=sc.nextInt();
+		List<Integer> list = new ArrayList();
+		for(int i=0; i<N; i++) {
+			list.add(sc.nextInt());	
+		}
+		list.sort(null);
+		int a=0;
+		for(int i=0; i<N; i++) {
+			a=a+list.get(i)*(N-i);
+		}
+		System.out.println(a);
+
+	}
+
+}
