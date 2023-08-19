b7cc7f372a40893bc9e63a6afb09af125bf872c1
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2023-08-19 10:42:56 +0900

    [Gold II] Title: 트리의 지름, Time: 1108 ms, Memory: 118328 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -0,0 +1,109 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Node {
+  int N;
+  boolean visit;
+
+  Node() {
+  };
+
+  Node(int N) {
+    this.N = N;
+  }
+
+  ArrayList<Node> adj = new ArrayList<Node>();
+  ArrayList<Integer> weights = new ArrayList<Integer>();
+
+}
+
+class Main {
+  public static Node[] nodes;
+  public static int[][] weight;
+  public static int[] treeLength;
+  public static Queue<Node> queue = new LinkedList<Node>();
+
+  public static void bfs(Node node) {
+    queue.add(node);
+    node.visit = true;
+    int s = 1;
+    while (!queue.isEmpty()) {
+      // System.out.println((s) + "번 실행되었습니다.");
+      // System.out.println(queue.size());
+      s++;
+      Node sNode = queue.poll();
+      for (int i=0; i<sNode.adj.size(); i++) {
+        Node n = sNode.adj.get(i);
+        if (!n.visit) {
+          n.visit = true;
+          queue.add(n);
+          treeLength[n.N] = Math.max(sNode.weights.get(i) + treeLength[sNode.N], treeLength[n.N]);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+    int n = Integer.parseInt(str);
+    String[] inputNo;
+    nodes = new Node[n];
+    treeLength = new int[n];
+    for (int i = 0; i < n; i++) {
+      nodes[i] = new Node(i);
+    }
+
+    for (int i = 0; i < n; i++) {
+      str = br.readLine();
+      inputNo = str.trim().split(" ");
+      int p = Integer.parseInt(inputNo[0])-1;
+      int q =1;
+      while(Integer.parseInt(inputNo[q])!=-1){
+      
+      int x = Integer.parseInt(inputNo[q]) - 1;
+      int w = Integer.parseInt(inputNo[q+1]);
+      nodes[p].adj.add(nodes[x]);
+      nodes[x].adj.add(nodes[p]);
+      nodes[p].weights.add(w);
+      nodes[x].weights.add(w);
+        q+=2;
+      }
+
+    }
+
+    // r (int i = 0; i < n; i++) {
+    // (nodes[i].adj.size() == 1) {
+    // bfs(nodes[i]);
+    // break;
+
+    // }
+    // }
+    bfs(nodes[0]);
+    int x = 0;
+    int max = 0;
+
+    for (int i = 0; i < n; i++) {
+      nodes[i].visit = false;
+      if (max <= treeLength[i]) {
+        x = i;
+        max = treeLength[i];
+      }
+    }
+
+    treeLength = new int[n];
+    bfs(nodes[x]);
+
+    for (int i = 0; i < n; i++) {
+      if (max <= treeLength[i]) {
+        max = treeLength[i];
+      }
+    }
+
+    System.out.println(max);
+
+  }
+}
