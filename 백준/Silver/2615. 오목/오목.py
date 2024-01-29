56e66071b570ca6c0609a1e1a418376b6cf9755a
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-01-29 20:41:02 +0900

    [Silver I] Title: 오목, Time: 108 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -10,10 +10,8 @@ def search(x, y):
             count+=1
         else:
             break
-    # if x==9 and y == 6:
-    # #     print(x, y, "실행1", count)
-    # # # print(count)
-    if count == 5:
+
+    if count == 5 and (x==0 or matrix[x-1][y]!=matrix[x][y]):
         return True
     else:
         return False
@@ -27,9 +25,7 @@ def search1(x, y):
             count+=1
         else:
             break
-    # if y == 18:
-    #     print("실행2", x, y, count)
-    if count == 5:
+    if count == 5  and (y==0 or matrix[x][y-1]!=matrix[x][y]):
         return True
     else:
         return False
@@ -43,10 +39,7 @@ def search2(x, y):
             count+=1
         else:
             break
-    # print(count)
-    # if y == 18:
-    #     print(x,y,count,"실행3")
-    if count == 5:
+    if count == 5 and (x==0 or y==0 or matrix[x][y]!=matrix[x-1][y-1]):
         return True
     else:
         return False
@@ -60,10 +53,7 @@ def search3(x, y):
             count+=1
         else:
             break
-    # print(count)
-    # if y == 18:
-    #     print(x,y,count,"실행4")
-    if count == 5:
+    if count == 5 and (y==18 or x==0 or matrix[x][y]!=matrix[x-1][y+1]):
         return True
     else:
         return False
@@ -76,27 +66,14 @@ y = 0
 for i in range(19):
     marker = False
     for j in range(19):
-        # if i==9 and j==6:
-        #     # print("ㅁㅇㄴㄻㄴㅇㄹ")
-        #     print(matrix[i][j], search(x, y), (i==0 or matrix[i-1][j]!=matrix[i][j]))
 
-        if matrix[i][j]!=0 and ((search(i, j) and (i==0 or matrix[i-1][j]!=matrix[i][j]))\
-                                or (search1(i, j) and (j==0 or matrix[i][j-1]!=matrix[i][j])) \
-                                or (search2(i, j) and (i==0 or j==0 or matrix[i][j]!=matrix[i-1][j-1]))\
-                                or (search3(i, j) and (j==18 or i==0 or matrix[i][j]!=matrix[i-1][j+1]))):
-            # print(i, j, "실행되었습니다")
-            # print(matrix[i][j], search(x, y), (i == 0 or matrix[i - 1][j] != matrix[i][j]))
-            # print(matrix[i][j], search1(x, y), (i == 0 or matrix[i - 1][j] != matrix[i][j]))
-            # print(matrix[i][j], search2(x, y), (i == 0 or matrix[i - 1][j] != matrix[i][j]))
-            # print(matrix[i][j], search3(x, y), (i == 0 or matrix[i - 1][j] != matrix[i][j]))
-
+        if matrix[i][j]!=0 and (search(i, j) or search1(i, j) or search2(i, j) or search3(i, j)):
             marker = True
             x = i
             y = j
             winner = matrix[i][j]
             break
     if marker:
-        # print(x, y, "끊음")
         break
 if winner != 0 and not search3(x, y):
     print(winner)
