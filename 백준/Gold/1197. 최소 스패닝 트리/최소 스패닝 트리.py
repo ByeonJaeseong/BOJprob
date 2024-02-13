369e1e8a49cd46b413130fc4de79f418ae137527
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-02-13 14:32:10 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 384 ms, Memory: 123720 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -15,10 +15,12 @@ matrix = [list(map(int, input().split())) for _ in range(N)]
 group = [i for i in range(M+1)]
 matrix.sort(key = lambda x : (x[2]))
 count = 0
+breaker = 0
 for i in range(N):
     if find(matrix[i][0])!=find(matrix[i][1]):
         union(matrix[i][0], matrix[i][1])
         count += matrix[i][2]
+        breaker+=1
+    if breaker==M-1:
+        break
 print(count)
-
-
