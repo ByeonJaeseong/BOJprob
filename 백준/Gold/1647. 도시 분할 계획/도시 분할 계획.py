38b34f1b594c344e16126f4c0e2241cf4a8dad31
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-02-13 14:47:26 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2740 ms, Memory: 272756 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,29 @@
+# import sys
+# sys.stdin=open("input.txt",'r')
+
+
+def find(n):
+    global group
+    if group[n]!=n:
+        group[n] = find(group[n])
+    return group[n]
+
+def union(a, b):
+    global group
+    group[find(b)] = find(a)
+
+M, N = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+group = [i for i in range(M+1)]
+matrix.sort(key = lambda x : (x[2]))
+count = 0
+breaker = 0
+if N>2:
+    for i in range(N):
+        if find(matrix[i][0])!=find(matrix[i][1]):
+            union(matrix[i][0], matrix[i][1])
+            count += matrix[i][2]
+            breaker+=1
+        if breaker==M-2:
+            break
+print(count)
