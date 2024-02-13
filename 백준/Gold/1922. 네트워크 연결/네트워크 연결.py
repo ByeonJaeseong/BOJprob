e63ef706e668a07dab70be7b7ece5a1039a1990a
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-02-13 20:08:16 +0900

    [Gold IV] Title: 네트워크 연결, Time: 300 ms, Memory: 122712 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,29 @@
+
+def find(n):
+    global group
+    # print(n)
+    if group[n]!=n:
+        group[n] = find(group[n])
+    return group[n]
+
+def union(a, b):
+    global group
+    group[find(b)] = find(a)
+
+M = int(input())
+N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+group = [i for i in range(M+1)]
+matrix.sort(key = lambda x : (x[2]))
+count = 0
+breaker = 0
+for i in range(N):
+    if find(matrix[i][0])!=find(matrix[i][1]):
+        union(matrix[i][0], matrix[i][1])
+        count += matrix[i][2]
+        breaker+=1
+    if breaker==M-1:
+        break
+print(count)
+
+
