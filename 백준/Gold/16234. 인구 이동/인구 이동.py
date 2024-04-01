53171bebcd2c2350106a24d6696a926c40792e41
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-04-01 15:12:12 +0900

    [Gold IV] Title: 인구 이동, Time: 964 ms, Memory: 133308 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,53 +1,45 @@
 '''
-얘부터 풀기로 결정
-구상 3분 -> BFS로 구현하기로 함
+문제 시작 2시 52분
 '''
+
 from collections import deque
 
-def check(x,y):
-    return 0<=x<N and 0<=y<N
-
-def bfs(x,y):
-    global visit
-    dq =deque()
-    dq.append((x,y))
-    result = []
-    sm = 0
-    while dq:
-        # print(*visit,sep='\n')
-        # print()
-        p, q = dq.popleft()
-        sm += matrix[p][q]
-        result.append((p,q))
-        for i in range(4):
-            X = p+dx[i]
-            Y = q+dy[i]
-            if check(X,Y) and not visit[X][Y] and MN<=abs(matrix[X][Y]-matrix[p][q])<=MX:
-                visit[X][Y] = True
-                dq.append((X,Y))
-    return [sm, result]
-
+def check(x, y) : return 0<=x<N and 0<=y<N
 
 N, MN, MX = map(int, input().split())
 matrix = [list(map(int, input().split())) for _ in range(N)]
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-count = 0
-while True:
-    # print(count)
+time = 0
+dx = (-1, 0, 1, 0)
+dy = (0, 1, 0, -1)
+for t in range(1, 2001):
     marker = False
+    new = [[-1]*N for _ in range(N)]
     visit = [[False]*N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if not visit[i][j]:
-                visit[i][j]=True
-                sm, result = bfs(i, j)
-                if len(result) == 1: continue
-                marker = True
-                value = sm//len(result)
-                for x, y in result:
-                    matrix[x][y]=value
-    # print(*matrix, sep='\n')
-    if marker: count+=1
-    else: break
-print(count)
+            if new[i][j] == -1 :
+                count = 1
+                sm = matrix[i][j]
+                dq = deque()
+                dq.append((i,j))
+                visit[i][j] =True
+                new_lst = [(i,j)]
+                while dq:
+                    p, q = dq.popleft()
+                    for k in range(4):
+                        X, Y = p+dx[k], q+dy[k]
+                        if check(X,Y) and not visit[X][Y] and MN<=abs(matrix[X][Y] - matrix[p][q])<=MX:
+                            new_lst.append((X,Y))
+                            visit[X][Y] = True
+                            count+=1
+                            sm +=matrix[X][Y]
+                            dq.append((X,Y))
+                if count > 1 : marker =True
+                # 인구 이동 일어났다고 체크
+                while new_lst:
+                    x, y = new_lst.pop()
+                    new[x][y] = sm // count
+    matrix = new
+    if not marker: break
+    time = t
+print(time)
