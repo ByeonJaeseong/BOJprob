b7278c07f702835228054dcd4da3714e2c57f313
ByeonJaeseong <102941775+ByeonJaeseong@users.noreply.github.com>
2024-04-11 20:14:11 +0900

    [Gold IV] Title: 테트로미노, Time: 192 ms, Memory: 115252 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,30 +1,42 @@
+def solve(blocks, sm, cnt):
+    global ans
 
-def check(x,y) : return 0<=x<M and 0<=y<N
-
-def tetris(x,y, value, lst = []):
-    global mx
-    if len(lst) == 4:
-        mx = max(mx, value)
+    if sm + (4 - cnt) * mx <= ans:
         return
 
-    if (4-len(lst))*mx_value+value<=mx:
+    if cnt == 4:
+        ans = max(ans, sm)
         return
 
-    for p, q in lst:
-        for i in range(4):
-            X = p + dx[i]
-            Y = q + dy[i]
-            if check(X,Y) and (X,Y) not in lst:
-                tetris(X,Y, value+matirx[X][Y], lst+[(X,Y)])
+    # blocks.sort()
+    # if str(blocks) in v_set:
+    #     return
+    # else:
+    #     v_set.add(str(blocks))
 
+    for b in blocks:
+        bi, bj = divmod(b, M)
+        for d in range(3):
+            ni, nj = bi + di[d], bj + dj[d]
+            if not (0 <= ni < N and 0 <= nj < M): continue
+            if v[ni][nj]: continue
 
-M, N = map(int, input().split())
-matirx = [list(map(int, input().split())) for _ in  range(M)]
-dx = (1, 0, -1, 0)
-dy = (0, 1, 0, -1)
-mx = 0
-mx_value = max([max(m) for m in matirx])
-for i in range(M):
-    for j in range(N):
-        tetris(i, j, matirx[i][j], [(i,j)])
-print(mx)
+            v[ni][nj] = 1
+            solve(blocks + [ni * M + nj], sm + arr[ni][nj], cnt+1)
+            v[ni][nj] = 0
+
+    return
+
+di = [0, 0, 1, -1]
+dj = [1, -1, 0, 0]
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+mx = max([max(a) for a in arr])
+ans = 0
+# v_set = set()
+v = [[0] * M for _ in range(N)]
+for i in range(N):
+    for j in range(M):
+        v[i][j] = 1
+        solve([i * M + j], arr[i][j], 1)
+print(ans)
